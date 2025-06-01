ced20eb2a57161a4df0072567e0cbd410ffe5251
frankiep95 <38260710+frankiep95@users.noreply.github.com>
2025-05-31 22:40:50 -0400

    Create FuelPanel.ino

---
+++ Left Console/FuelPanel/FuelPanel.ino
@@ -0,0 +1,134 @@
+/**************************************************************************************
+ *        ____                   _    _                       _
+ *       / __ \                 | |  | |                     | |
+ *      | |  | |_ __   ___ _ __ | |__| | ___  _ __ _ __   ___| |_
+ *      | |  | | '_ \ / _ \ '_ \|  __  |/ _ \| '__| '_ \ / _ \ __|
+ *      | |__| | |_) |  __/ | | | |  | | (_) | |  | | | |  __/ |_
+ *       \____/| .__/ \___|_| |_|_|  |_|\___/|_|  |_| |_|\___|\__|
+ *             | |
+ *             |_|
+ *   ----------------------------------------------------------------------------------
+ *   Copyright 2016-2024 OpenHornet
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *   ----------------------------------------------------------------------------------
+ *   Note: All other portions of OpenHornet not within the 'OpenHornet-Software' 
+ *   GitHub repository is released under the Creative Commons Attribution -
+ *   Non-Commercial - Share Alike License. (CC BY-NC-SA 4.0)
+ *   ----------------------------------------------------------------------------------
+ *   This Project uses Doxygen as a documentation generator.
+ *   Please use Doxygen capable comments.
+ **************************************************************************************/
+
+/**
+ * @file OHSketchTemplate.ino
+ * @author <Replace with author's name>
+ * @date <MM.DD.YYYY>
+ * @version u.0.0.1 (untested)
+ * @copyright Copyright 2016-2024 OpenHornet. Licensed under the Apache License, Version 2.0.
+ * @warning This sketch is based on a wiring diagram, and was not yet tested on hardware. (Remove this line once tested on hardware and in system.)
+ * @brief <A short description of the PCB, will appear in the list of files.>
+ *
+ * @details This is the OpenHornet Sketch Template. It should be used as a starting point for every new sketch.
+ * Please copy the whole OHSketchTemplate folder to start, and ensure the correct line in the Makefile is uncommented.
+ * 
+ *  * **Reference Designator:** <Replace with full reference designator.>
+ *  * **Intended Board:** <Replace with the appropriate hardware, i.e. ABSIS ALE, ABSIS ALE w/ Relay Module, etc.>
+ *  * **RS485 Bus Address:** <NA or RS485 Bus Address, i.e. 1, 2, 3, etc.>
+ * 
+ * ### Wiring diagram:
+ * PIN | Function
+ * --- | ---
+ * 1   | function 1
+ * 2   | function 2
+ * 3   | function 3
+ * 
+ * @brief The following #define tells DCS-BIOS that this is a RS-485 slave device.
+ * It also sets the address of this slave device. The slave address should be
+ * between 1 and 126 and must be unique among all devices on the same bus.
+ *
+ * @bug Currently does not work with the Pro Micro (32U4), Fails to compile. 
+
+   #define DCSBIOS_RS485_SLAVE 1 ///DCSBios RS485 Bus Address, once bug resolved move line below comment.
+*/
+
+/**
+ * Check if we're on a Mega328 or Mega2560 and define the correct
+ * serial interface
+ * 
+ */
+
+
+#define MUXADDRESS 5
+#define DCSBIOS_MUX_SERIAL
+#include "DcsBios.h"
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+
+
+// Define pins for DCS-BIOS per interconnect diagram.
+#define PROBE_SW1 15 
+#define PROBE_SW2 6 
+#define WING_SW1  14
+#define WING_SW2  7
+#define CTR_SW1   16
+#define CTR_SW2   8
+#define DUMP_SW1  10
+#define COIL1     3
+
+DcsBios::Switch3Pos probeSw("PROBE_SW", PROBE_SW1, PROBE_SW2);
+DcsBios::Switch3Pos extWngTankSw("EXT_WNG_TANK_SW", WING_SW1, WING_SW2);
+DcsBios::Switch3Pos extCntTankSw("EXT_CNT_TANK_SW", CTR_SW1, CTR_SW2);
+DcsBios::Switch2Pos fuelDumpSw("FUEL_DUMP_SW", DUMP_SW1);
+
+/**
+* Arduino Setup Function
+*
+* Arduino standard Setup Function. Code who should be executed
+* only once at the programm start, belongs in this function.
+*/
+void setup() {
+
+pinMode(COIL1, OUTPUT);
+digitalWrite(COIL1, LOW);
+  // Run DCS Bios setup function
+  DcsBios::setup();
+}
+
+/**
+* Arduino Loop Function
+*
+* Arduino standard Loop Function. Code who should be executed
+* over and over in a loop, belongs in this function.
+*/
+unsigned int value = 0;
+void loop() {
+
+  //Run DCS Bios loop function
+  DcsBios::loop();
+ value = DcsBios::CheckBus("74b4");
+  checkCoil();
+
+}
+
+void checkCoil(){
+  if(digitalRead(DUMP_SW1) == HIGH || value == 1){
+    digitalWrite(COIL1,HIGH);
+  }
+  if(value == 0){
+    digitalWrite(COIL1,LOW);
+  }
+
+}
